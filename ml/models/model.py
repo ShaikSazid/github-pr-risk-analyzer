0bf88173d74d04bdfc04aeec5a8a5949c4648c5f
xevansz <minithbmatthew@gmail.com>
2026-04-09 20:19:40 +0530

    fix: unused imports in model

--- ml/models/model.py
+++ ml/models/model.py
@@ -1,10 +1,3 @@
-# TODO 
-# Build Random Forest Classifier
-# Train it on X_train and y_train
-# Evaluate on X_test and y_test
-# Get feature importance rankings
-
-from tkinter.constants import N
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
@@ -19,15 +12,12 @@ X = df.drop("is_risky", axis=1)
 y = df["is_risky"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-  X, y, test_size=0.2, random_state=42, stratify=y
+    X, y, test_size=0.2, random_state=42, stratify=y
 )
 
 # train
 model = RandomForestClassifier(
-  n_estimators=200,
-  random_state=42,
-  class_weight="balanced",
-  n_jobs=1
+    n_estimators=200, random_state=42, class_weight="balanced", n_jobs=1
 )
 model.fit(X_train, y_train)
 
@@ -42,14 +32,12 @@ print(f"ROC AUC: , {roc_auc_score(y_test, y_prob):.4f}")
 
 # feature importances
 importances = sorted(
-  zip(X.columns, model.feature_importances_),
-  key=lambda x:x[1],
-  reverse=True
+    zip(X.columns, model.feature_importances_), key=lambda x: x[1], reverse=True
 )
 
 print("Important features")
 for feat, imp in importances:
-  print(f"{feat}: {imp}")
+    print(f"{feat}: {imp}")
 
 joblib.dump(model, "rf_model.joblib")
 joblib.dump(list(X.columns), "feature_columns.joblib")
